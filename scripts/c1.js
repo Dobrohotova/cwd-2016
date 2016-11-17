86aa654eaa92f0bd706f77a3e836068f44f9c875
Dobrohotova <irina.dobrohotova@gmail.com>
2016-11-17 12:35:47 +0100

    Java Script first class

---
+++ scripts/c1.js
@@ -0,0 +1,75 @@
+// var ime = 'Bojan'; //string
+// var vozrast = 34; //number (integer)
+// var visina = 182.4; //number (float)
+// var vozachka_dozvola = true; //boolean
+
+// //vozachka_dozvola - snake case
+// //vozachkaDozvola - camel case
+
+// console.log('Zdravo Svetu!');
+
+// console.log(ime);
+// console.log(vozrast);
+// console.log(visina);
+// console.log(vozachka_dozvola);
+
+// console.debug('Debugging some variables..');
+// console.error('My name is not' + ime);
+// console.info('This guy is this high ' + visina + 'cm');
+
+// var grad = 'Skopje';
+// var nacionalnost = 'Makedonka';
+// var kola = 'Nubira';
+// var cena = 200000000;
+// var ulica = 'Nikola Trimpare';
+// var single = true;
+// var fakultet = true;
+// var zanimanje = 'Ekonomist';
+// var hobi = 'plivanje';
+// var rodenden = '08.09.1992'; 
+// var gender = 'female';
+// var kola_boja = 'zlatna';
+// var ochi_boja = 'kafeavi';
+// var kosa_boja = 'crvena';
+// var prezime = 'Dobrohotova';
+// var broj_na_ulica = '24';
+// var indeks = '54489';
+// var godini = '24';
+// var 
+// var 
+
+// var g = 20;
+// var h = 3;
+
+// console.log(g % h);
+
+function zdravo(){
+	console.log('ZdravoSvetu');
+}
+
+zdravo();
+
+function pozdrav(ime){
+	console.log('Zdravo ' + ime);
+}
+
+pozdrav('Pero');
+pozdrav('Janko');
+pozdrav('Stanko');
+pozdrav('Branko');
+
+
+
+
+
+function f2c(farenheit){
+	console.log((farenheit - 32) / 1.8);
+}
+
+function c2f(celsius){
+	console.log(celsius * 1.8 + 32);
+}
+
+f2c();
+
+c2f();
