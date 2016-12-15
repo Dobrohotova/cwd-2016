e73c81c365ddaa96e9a1b2337b769ed267007bdf
Dobrohotova <irina.dobrohotova@gmail.com>
2016-12-15 13:06:02 +0100

    add up .value

--- scripts/c4.js
+++ scripts/c4.js
@@ -13,17 +13,25 @@
 // };
 
 
-for(let i = 100; i > 0; i = i - 3){
-	if(i % 2 == 0){
-		console.log(i);
-	};
-};
+// for(let i = 100; i > 0; i = i - 3){
+// 	if(i % 2 == 0){
+// 		console.log(i);
+// 	};
+// };
 
-for(let i = 0; i < thumbs.lenth; i++){
-	// console.log(thumbs[i]);
-	thumbs[i].addEvenetListener('click', function(){
-		console.log(this);
-	});
-}
+// for(let i = 0; i < thumbs.lenth; i++){
+// 	// console.log(thumbs[i]);
+// 	thumbs[i].addEvenetListener('click', function(){
+// 		console.log(this);
+// 	});
+// }
 
+var tb1 = document.querySelector('.textbox1');
+var tf1 = document.querySelector('.textfield1');
+var tf2 = document.querySelector('.textfield2');
+
+tb1.addEvenetListener('keydown', function(e){
+	tf1.innerText = tb1.value;
+	tf2.innerHTML = tb1.value;
+});
 
