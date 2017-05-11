894301d873acbca774846bbb8aec62dd98512046
Dobrohotova <irina.dobrohotova@gmail.com>
2017-05-11 10:58:16 +0200

    correct homework

--- scripts/test2.js
+++ scripts/test2.js
@@ -8,37 +8,37 @@ function loadItems(loading){
 			   result.json().then(function(data){	
 			   			for(var i = 0; i < data.length; i++){
 				   				var li = document.createElement('li');
-								var span = document.createElement('span');
+								var p = document.createElement('p');
 								var h4 = document.createElement('h4');
 
 								h4.innerText = data[i].title;
-								span.innerText = data[i].body;
-								span.style.display = 'none';
+								p.innerText = data[i].body;
+								// p.style.display = 'none';
 
 								li.addEventListener('click', function(klik){
-									klik.preventDefault();
-									var spa = this.querySelectorAll('span')[0];
+									// klik.preventDefault();
+									this.querySelector("p").className = "visible";
 									// spa.style.transition = 'all .3s ease-in-out';
 									// spa.style.fontSize = '13px';
-									var style = spa.style;
-									if(style.display === 'none'){
-										style.display = '';
-									} else {
-										style.display = 'none';
-									}
+									// var style = spa.style;
+									// if(style.display === 'none'){
+									// 	style.display = '';
+									// } else {
+									// 	style.display = 'none';
+									// }
 
 									// document.querySelector("p").className = "visible";
-									// console.log(div);
+
 
 								});
 
 								li.appendChild(h4);
-								li.appendChild(span);
+								li.appendChild(p);
 								ul.appendChild(li);
 
-								// span.addEventListener('click', function(){
-								// 		this.style.display = 'none';
-								// 	});
+									p.addEventListener('click', function(){
+											this.style.display = 'none';
+										});
 						}
 
 
